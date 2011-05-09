0b0b5528995c5fe626e62a5c579cca8fb915ae37
John Nolan <john.nolan@moneyplusgroup.co.uk>
2011-05-09 15:32:33 +0100

    initial commit

---
+++ TeamCityExample.Tests/HelloWorldTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TeamCityExample.Tests
+{
+    [TestFixture]
+    public class HelloWorldTest
+    {
+        [Test]
+        public void TestSayHi()
+        {
+            var helloWorld = new HelloWorld();
+            Assert.That("Hello: world",Is.EqualTo(helloWorld.SayHi("world")));
+        }
+
+    }
+}
---
+++ TeamCityExample/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamCityExample
+{
+    public class HelloWorld
+    {
+        public string SayHi(string hello)
+        {
+            return "Hello: "+ hello;
+        }
+    }
+}
